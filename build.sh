71603943cca26a37e479a3676e3f92c59594c4b9
Mateus Nepomuceno <mateusnepomuceno@gmail.com>
2026-02-10 16:44:19 -0300

    feat: adicionando build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collecstatic --no-input
+
+python manage.py migrate
+
+if [ "$SUPERUSER_NAME" ]; then
+  python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$SUPERUSER_NAME').exists():
+    User.objects.create_superuser('$SUPERUSER_NAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')
+    print("Superuser criado com sucesso!")
+else:
+    print("Superuser já existe.")
+END
+fi
